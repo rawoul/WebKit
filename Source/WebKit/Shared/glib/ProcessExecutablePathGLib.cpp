c1646c8a9fd9f3b544287bcb4d59fb995758dd01
Théo Maillart <tmaillart@freebox.fr>
2023-02-15 11:41:05 +0100

    [fbx] enable overriding exec process dir path

--- Source/WebKit/Shared/glib/ProcessExecutablePathGLib.cpp
+++ Source/WebKit/Shared/glib/ProcessExecutablePathGLib.cpp
@@ -44,7 +44,6 @@ static String getExecutablePath()
 
 static String findWebKitProcess(const char* processName)
 {
-#if ENABLE(DEVELOPER_MODE)
     static const char* execDirectory = g_getenv("WEBKIT_EXEC_PATH");
     if (execDirectory) {
         String processPath = FileSystem::pathByAppendingComponent(FileSystem::stringFromFileSystemRepresentation(execDirectory), StringView::fromLatin1(processName));
@@ -52,6 +51,7 @@ static String findWebKitProcess(const char* processName)
             return processPath;
     }
 
+#if ENABLE(DEVELOPER_MODE)
     static String executablePath = getExecutablePath();
     if (!executablePath.isNull()) {
         String processPath = FileSystem::pathByAppendingComponent(executablePath, StringView::fromLatin1(processName));
