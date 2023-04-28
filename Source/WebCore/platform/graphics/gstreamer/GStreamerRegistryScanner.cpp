64ce588d09ea4d0264a010a124d5f92480805207
Arnaud Vrac <avrac@freebox.fr>
2023-04-28 18:08:39 +0200

    Convert features data to lowercase before checking support

--- Source/WebCore/platform/graphics/gstreamer/GStreamerRegistryScanner.cpp
+++ Source/WebCore/platform/graphics/gstreamer/GStreamerRegistryScanner.cpp
@@ -624,7 +624,7 @@ bool GStreamerRegistryScanner::supportsFeatures(const String& features) const
 {
     // Apple TV requires this one for DD+.
     constexpr auto dolbyDigitalPlusJOC = "joc"_s;
-    if (features == dolbyDigitalPlusJOC)
+    if (features.convertToASCIILowercase() == dolbyDigitalPlusJOC)
         return true;
 
     return false;
